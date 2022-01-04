e6a7dae3b2ce2c56eb0b3850f3a971a7cc32ac9a
Mahmudur <mahmudhera93@gmail.com>
2022-01-05 00:05:43 +0600

    added basic code

--- expt01/main.py
+++ expt01/main.py
@@ -2,10 +2,11 @@ import dna_jellyfish as jellyfish
 
 def get_off_target_count(target_jf_file, genome_jf_file, kmer_str):
 	mer = jellyfish.MerDNA(kmer_str)
-	return genome_jf_file[mer]
+	mer = mer.canonicalize()
+	return max(0, genome_jf_file[mer]-target_jf_file[mer])
 
 if __name__ == "__main__":
-	target_jf_filename = 'staphylococcus_genome.jf'
+	target_jf_filename = 'mer_counts.jf'
 	genome_jf_filename = 'staphylococcus_genome.jf'
 	test_kmer = 'AGTCGTCGTCGTACGTGCGGG'
 	
