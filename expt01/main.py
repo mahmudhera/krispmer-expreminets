1d4d602cfabe653d3c5ec143c6bff572c559a97d
Mahmudur <mahmudhera93@gmail.com>
2022-01-04 23:54:54 +0600

    added basic code

--- expt01/main.py
+++ expt01/main.py
@@ -1,2 +1,17 @@
+import dna_jellyfish as jellyfish
+
+def get_off_target_count(target_jf_file, genome_jf_file, kmer):
+	return 0
+
 if __name__ == "__main__":
-	print("hi")
+	target_jf_filename = 'test'
+	genome_jf_filename = 'test'
+	test_kmer = 'AGTCGTCGTCGTACGTGCGGG'
+	
+	target_qf = jellyfish.QueryMerFile(target_jf_filename)
+	genome_qf = jellyfish.QueryMerFile(target_jf_filename)
+	
+	x = get_off_target_count(target_qf, genome_qf, test_kmer)
+	print (x)
+	
+	print("compiles ok")
