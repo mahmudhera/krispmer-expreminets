ed4ede4d200100aa447876e4c38731888a5b5f45
Mahmudur <mahmudhera93@gmail.com>
2022-01-05 23:57:48 +0600

    updated

--- expt01/main.py
+++ expt01/main.py
@@ -18,19 +18,19 @@ def generate_jf_file(fasta_filename, jf_filename="temp"):
 	subprocess.call(args)
 	return jellyfish.QueryMerFile(jf_filename)
 	
-def get_list_of_grnas(scores_filename):
-	l = pd.read_csv(scores_filename)['tgt_in_plus'].to_list()
-	print (l)
-
+def get_list_of_grna_strings(scores_filename):
+	return pd.read_csv(scores_filename)['tgt_in_plus'].to_list()
+	
 if __name__ == "__main__":
 	genome_jf_filename = 'staphylococcus_genome.jf'
 	test_kmer = 'CCAATTGGGGCCGTCTCTATAAT'
 	target_qf = generate_jf_file("../../../data/staphylococcusAureus/target1")
-	get_list_of_grnas("../../../data/staphylococcusAureus/scores1")
+	grnas = get_list_of_grna_strings("../../../data/staphylococcusAureus/scores1")
 	
 	genome_qf = jellyfish.QueryMerFile(genome_jf_filename)
 	
-	x = get_off_target_count(target_qf, genome_qf, test_kmer)
-	print (x)
+	for grna in grnas:
+		x = get_off_target_count(target_qf, genome_qf, test_kmer)
+		print (x)
 	
 	print("compiles ok")
