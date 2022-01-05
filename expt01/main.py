251c9bee98f2895c2d3f8760c4fb0657ef405ebd
Mahmudur <mahmudhera93@gmail.com>
2022-01-05 22:55:56 +0600

    updated

--- expt01/main.py
+++ expt01/main.py
@@ -2,10 +2,11 @@ import dna_jellyfish as jellyfish
 
 def get_off_target_count(target_jf_file, genome_jf_file, kmer_str):
 	mer = jellyfish.MerDNA(kmer_str)
-	print(genome_jf_file[mer], target_jf_file[mer])
+	cg1, ct1 = genome_jf_file[mer], target_jf_file[mer]
 	mer.canonicalize()
-	print(genome_jf_file[mer], target_jf_file[mer])
-	return max(0, genome_jf_file[mer]-target_jf_file[mer])
+	cg2, ct2 = genome_jf_file[mer], target_jf_file[mer]
+	print(cg1 + cg2, ct1 + ct2)
+	return max(0, cg1 + cg2 - ct1 - ct2)
 	
 #def generate_jf_file(fasta_filename):
 	# given a fasta file, use jellyfish to count 23-mers
