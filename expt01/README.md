996aecdfd232b806402c8aab42b45f114205faeb
Mahmudur <mahmudhera93@gmail.com>
2022-01-05 22:52:48 +0600

    updated

--- expt01/README.md
+++ expt01/README.md
@@ -4,3 +4,7 @@ The steps are as follows:
 
 1. We will write a python code that can calculate the number of off-target cuts given a gRNA
 1. Others...
+
+Need to do in the future:
+1. We have to rewrite tool so that (a) canonicalize is resolved, and (b) -1 scored gRNAs are not selected
+1. Next, we will also need to install this tool in another machine, and then make runs there
--- expt01/main.py
+++ expt01/main.py
@@ -2,9 +2,14 @@ import dna_jellyfish as jellyfish
 
 def get_off_target_count(target_jf_file, genome_jf_file, kmer_str):
 	mer = jellyfish.MerDNA(kmer_str)
+	print(genome_jf_file[mer], target_jf_file[mer])
 	mer.canonicalize()
 	print(genome_jf_file[mer], target_jf_file[mer])
 	return max(0, genome_jf_file[mer]-target_jf_file[mer])
+	
+def generate_jf_file(fasta_filename):
+	# given a fasta file, use jellyfish to count 23-mers
+	# returns a QueryMerFile
 
 if __name__ == "__main__":
 	target_jf_filename = 'mer_counts.jf'
