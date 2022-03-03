c3641ef91b6772cb274bdbc5ab5b3da97ab778ec
Mahmudur <mahmudhera93@gmail.com>
2022-03-03 04:34:41 -0500

    test

--- expt01/main.py
+++ expt01/main.py
@@ -3,6 +3,8 @@ import subprocess
 import pandas as pd
 import sys
 
+# number of off-target counts of kmer_str in genome
+# requires two jf files
 def get_off_target_count(target_jf_file, genome_jf_file, kmer_str):
 	mer = jellyfish.MerDNA(kmer_str)
 	cg1, ct1 = genome_jf_file[mer], target_jf_file[mer]
@@ -11,27 +13,35 @@ def get_off_target_count(target_jf_file, genome_jf_file, kmer_str):
 	print (cg1+cg2, ct1+ct2)
 	return max(0, cg1 + cg2 - ct1 - ct2)
 
+# given a fasta file, use jellyfish to count 23-mers
+# returns a QueryMerFile
 def generate_jf_file(fasta_filename, jf_filename="temp"):
-	# given a fasta file, use jellyfish to count 23-mers
-	# returns a QueryMerFile
 	jf_command = "jellyfish count -m 23 -s 100M -o " + jf_filename + " -t 8 -C " + fasta_filename
 	args = jf_command.split(' ')
 	subprocess.call(args)
 	return jellyfish.QueryMerFile(jf_filename)
 
+# returns grnas in +ve strand
 def get_list_of_grna_strings(scores_filename):
 	return pd.read_csv(scores_filename)['tgt_in_plus'].to_list()
 
+
+
 if __name__ == "__main__":
-	if len(sys.argv) < 10:
+	if len(sys.argv) < 5:
 		print("error")
 		print("usage: python main.py working_direcory genome_jf_filename target_filename scores_filename max_hd")
 		sys.exit(-1)
 
-	print(sys.argv)
+	folder_name = sys.argv[1]
+	genome_jf_filename = sys.argv[2]
+	target_filename = sys.argv[3]
+	scores_filename = sys.argv[4]
+	max_hd = int(sys.argv[5])
 
-	folder_name = sys.argv[2]
-	genome_jf_filename = sys.argv[3]
-	target_filename = sys.argv[4]
-	scores_filename = sys.argv[5]
-	max_hd = int(sys.argv[6])
+	print(folder_name, target_filename)
+
+	#qf_target = generate_jf_file()
+	# create two jf files
+	# copy fn from github
+	# call that
