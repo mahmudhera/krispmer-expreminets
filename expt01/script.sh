e9e6a98415caaf2151bcd29dd1ce983b97efb27e
Mahmudur <mahmudhera93@gmail.com>
2022-03-05 19:57:10 -0500

    ran script

--- expt01/script.sh
+++ expt01/script.sh
@@ -1,10 +1,30 @@
+python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target1.fasta ../runs-on-genomes/staphylococcus-aureus/scores1 3 staphylococcus-aureus/out_for_target1
+echo done for 1
+python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target2.fasta ../runs-on-genomes/staphylococcus-aureus/scores2 3 staphylococcus-aureus/out_for_target2
+echo done for 2
+python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target3.fasta ../runs-on-genomes/staphylococcus-aureus/scores3 3 staphylococcus-aureus/out_for_target3
+echo done for 3
+python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target4.fasta ../runs-on-genomes/staphylococcus-aureus/scores4 3 staphylococcus-aureus/out_for_target4
+echo done for 4
+python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target5.fasta ../runs-on-genomes/staphylococcus-aureus/scores5 3 staphylococcus-aureus/out_for_target5
+echo done for 5
 python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target6.fasta ../runs-on-genomes/staphylococcus-aureus/scores6 3 staphylococcus-aureus/out_for_target6
+echo done for 6
 python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target7.fasta ../runs-on-genomes/staphylococcus-aureus/scores7 3 staphylococcus-aureus/out_for_target7
+echo done for 7
 python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target8.fasta ../runs-on-genomes/staphylococcus-aureus/scores8 3 staphylococcus-aureus/out_for_target8
+echo done for 8
 python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target9.fasta ../runs-on-genomes/staphylococcus-aureus/scores9 3 staphylococcus-aureus/out_for_target9
+echo done for 9
 python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target10.fasta ../runs-on-genomes/staphylococcus-aureus/scores10 3 staphylococcus-aureus/out_for_target10
+echo done for 10
 python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target11.fasta ../runs-on-genomes/staphylococcus-aureus/scores11 3 staphylococcus-aureus/out_for_target11
+echo done for 11
 python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target12.fasta ../runs-on-genomes/staphylococcus-aureus/scores12 3 staphylococcus-aureus/out_for_target12
+echo done for 12
 python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target13.fasta ../runs-on-genomes/staphylococcus-aureus/scores13 3 staphylococcus-aureus/out_for_target13
+echo done for 13
 python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target14.fasta ../runs-on-genomes/staphylococcus-aureus/scores14 3 staphylococcus-aureus/out_for_target14
+echo done for 14
 python main.py ../runs-on-genomes/staphylococcus-aureus/mer_counts.jf ../runs-on-genomes/staphylococcus-aureus/target15.fasta ../runs-on-genomes/staphylococcus-aureus/scores15 3 staphylococcus-aureus/out_for_target15
+echo done for 15
--- expt01/script_generator.py
+++ expt01/script_generator.py
@@ -1,9 +1,12 @@
-start = 6
+start = 1
 end = 15
+genome_name = 'staphylococcus-aureus'
 
 f = open('script.sh', 'w')
 for i in range(start, end+1):
     num = str(i)
-    str_ = 'python main.py ../runs-on-genomes/rhodobacter-sphaeroides/mer_counts.jf ../runs-on-genomes/rhodobacter-sphaeroides/target' + num + '.fasta ../runs-on-genomes/rhodobacter-sphaeroides/scores' + num + ' 3 rhodobacter-sphaeroides/out_for_target'+num
+    str_ = 'python main.py ../runs-on-genomes/' + genome_name + '/mer_counts.jf ../runs-on-genomes/' + genome_name + '/target' + num + '.fasta ../runs-on-genomes/' + genome_name + '/scores' + num + ' 3 ' + genome_name + '/out_for_target'+num
+    f.write(str_ + '\n')
+    str_ = "echo done for " + num
     f.write(str_ + '\n')
 f.close()
