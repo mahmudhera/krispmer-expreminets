f7a80aa45ad43233d79d590219892e7fd8018fe4
Mahmudur <mahmudhera93@gmail.com>
2022-04-14 00:16:18 -0400

    removed temporaries

--- runs_using_other_tools/guidescan2/staphylococcus-aureus/all_target_handler.py
+++
@@ -1,7 +0,0 @@
-num_targets = 15
-genome_index_name = 'genome.fasta.index'
-
-for i in range(1, num_targets+1):
-    i_str = str(i)
-    print('python kmer_generator_from_target.py target'+i_str+'.fasta > tmp')
-    print('guidescan enumerate -o target' + i_str + '_guidescan_out -f tmp ' + genome_index_name)
