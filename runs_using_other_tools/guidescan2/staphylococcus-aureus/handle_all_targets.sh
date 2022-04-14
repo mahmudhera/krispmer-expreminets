60f6caf3dce937d119f147b6187953920c057e16
atifrahman <atif.bd@gmail.com>
2022-04-13 20:20:49 -0700

    ok

---
+++ runs_using_other_tools/guidescan2/staphylococcus-aureus/handle_all_targets.sh
@@ -0,0 +1,30 @@
+python kmer_generator_from_target.py target1.fasta > tmp
+guidescan enumerate -o target1_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target2.fasta > tmp
+guidescan enumerate -o target2_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target3.fasta > tmp
+guidescan enumerate -o target3_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target4.fasta > tmp
+guidescan enumerate -o target4_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target5.fasta > tmp
+guidescan enumerate -o target5_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target6.fasta > tmp
+guidescan enumerate -o target6_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target7.fasta > tmp
+guidescan enumerate -o target7_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target8.fasta > tmp
+guidescan enumerate -o target8_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target9.fasta > tmp
+guidescan enumerate -o target9_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target10.fasta > tmp
+guidescan enumerate -o target10_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target11.fasta > tmp
+guidescan enumerate -o target11_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target12.fasta > tmp
+guidescan enumerate -o target12_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target13.fasta > tmp
+guidescan enumerate -o target13_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target14.fasta > tmp
+guidescan enumerate -o target14_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target15.fasta > tmp
+guidescan enumerate -o target15_guidescan_out -f tmp genome.fasta.index
---
+++ runs_using_other_tools/guidescan2/staphylococcus-aureus/script.sh
@@ -0,0 +1,30 @@
+python kmer_generator_from_target.py target1.fasta
+guidescan enumerate -o target1_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target2.fasta
+guidescan enumerate -o target2_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target3.fasta
+guidescan enumerate -o target3_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target4.fasta
+guidescan enumerate -o target4_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target5.fasta
+guidescan enumerate -o target5_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target6.fasta
+guidescan enumerate -o target6_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target7.fasta
+guidescan enumerate -o target7_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target8.fasta
+guidescan enumerate -o target8_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target9.fasta
+guidescan enumerate -o target9_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target10.fasta
+guidescan enumerate -o target10_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target11.fasta
+guidescan enumerate -o target11_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target12.fasta
+guidescan enumerate -o target12_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target13.fasta
+guidescan enumerate -o target13_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target14.fasta
+guidescan enumerate -o target14_guidescan_out -f tmp genome.fasta.index
+python kmer_generator_from_target.py target15.fasta
+guidescan enumerate -o target15_guidescan_out -f tmp genome.fasta.index
