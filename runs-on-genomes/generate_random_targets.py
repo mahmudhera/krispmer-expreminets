021162be3405472fc3acfba9abcd4675e210b011
Mahmudur <mahmudhera93@gmail.com>
2022-04-20 02:13:59 -0400

    added detailed name of chromosome

--- runs-on-genomes/generate_random_targets.py
+++ runs-on-genomes/generate_random_targets.py
@@ -11,14 +11,13 @@ hd = 3
 bash_file = open('script.sh', 'w')
 target_id = 1
 for seq_record in SeqIO.parse(fasta_filename, "fasta"):
-	print(seq_record.name)
-	continue
+	seq_long_id = seq_record.description.replace(" ", "_")
 	num_bases = len(seq_record)
 	start_positions = [random.randint(1,num_bases-target_len_high-1) for i in range(num_targets_each_chr)]
 	end_positions = [start_positions[i] + random.randint(target_len_low, target_len_high) for i in range(num_targets_each_chr)]
 	for pos in list(zip(start_positions, end_positions)):
 		#print(str(seq_record.id)+":"+str(pos[0])+"-"+str(pos[1]))
-		f = open('target' + str(target_id) + '.fasta', 'w')
+		f = open('target_' + seq_long_id + "_" + str(target_id) + '.fasta', 'w')
 		f.write ('> target' + str(target_id) + '\n')
 		f.write (str(seq_record.seq[pos[0]:pos[1]]) + '\n')
 		f.close()
