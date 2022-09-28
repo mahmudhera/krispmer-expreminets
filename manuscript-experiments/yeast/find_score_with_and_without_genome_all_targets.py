6206a70a406dbbac03091e4e09e052a43d5a8292
Mahmudur <mahmudhera93@gmail.com>
2022-09-28 16:15:28 -0400

    fasta files

--- manuscript-experiments/yeast/find_score_with_and_without_genome_all_targets.py
+++ manuscript-experiments/yeast/find_score_with_and_without_genome_all_targets.py
@@ -1,6 +1,10 @@
+import os
 
 def main():
-    print('Test')
+    list_grna_scores = []
+    for file os.listdir('.'):
+        if file.endswith('.fasta'):
+            print(file)
 
 if __name__ == "__main__":
     main()
