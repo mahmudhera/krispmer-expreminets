ab05bc08596dc9014726b009c08f611a1cd8f726
Mahmudur <mahmudhera93@gmail.com>
2022-09-28 16:22:20 -0400

    added code to analyze scores with genome

--- manuscript-experiments/yeast/find_score_with_and_without_genome_all_targets.py
+++ manuscript-experiments/yeast/find_score_with_and_without_genome_all_targets.py
@@ -1,10 +1,26 @@
 import os
+import subprocess
 
 def main():
     list_grna_scores = []
+
+    all_fasta_files = []
     for file in os.listdir('.'):
         if file.endswith('.fasta') and not file.startswith('scores'):
-            print(file)
+            all_fasta_files.append(file)
+
+    for filename in all_fasta_files:
+        tgt_fname = filename
+        genome_jf_fname = 'mer_counts.jf'
+        scores_fname = 'scores_' + tgt_fname
+        max_hd = '3'
+        out_fname = 'tmt_scores'
+
+        cmd_args = ['python', 'find_score_with_genome.py', genome_jf_fname, tgt_fname, scores_fname, max_hd, out_fname]
+        subprocess.call(cmd_args)
+
+        print(tgt_fname, scores_fname)
+        break
 
 if __name__ == "__main__":
     main()
