f13d5c5a95297e87996bcf466561870ac26f77e5
Mahmudur <mahmudhera93@gmail.com>
2022-09-28 16:16:29 -0400

    fixed typo

--- manuscript-experiments/yeast/find_score_with_and_without_genome_all_targets.py
+++ manuscript-experiments/yeast/find_score_with_and_without_genome_all_targets.py
@@ -3,7 +3,7 @@ import os
 def main():
     list_grna_scores = []
     for file in os.listdir('.'):
-        if file.endswith('.fasta'):
+        if file.endswith('.fasta') and not file.startswith('scores'):
             print(file)
 
 if __name__ == "__main__":
