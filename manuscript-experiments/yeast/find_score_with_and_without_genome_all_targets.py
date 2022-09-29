6a83bc3a96efe944b032e85d4509bc0b7cd97736
Mahmudur <mahmudhera93@gmail.com>
2022-09-28 22:44:26 -0400

    added gene files, fixed bug

--- manuscript-experiments/yeast/find_score_with_and_without_genome_all_targets.py
+++ manuscript-experiments/yeast/find_score_with_and_without_genome_all_targets.py
@@ -7,9 +7,11 @@ def main():
 
     all_fasta_files = []
     for file in os.listdir('.'):
-        if file.endswith('.fasta') and not file.startswith('scores'):
+        if file.endswith('.fasta') and not file.startswith('scores') and not file.startswith('genome'):
             all_fasta_files.append(file)
 
+    print('Handling ' + str(len(all_fasta_files)) + ' targets...')
+    c = 0
     for filename in all_fasta_files:
         tgt_fname = filename
         genome_jf_fname = 'genome_counts.jf'
@@ -28,10 +30,12 @@ def main():
             score_with_genome = float(line.split(' ')[1])
             score_without_genome = float(line.split(' ')[2])
             list_grna_scores.append((score_with_genome, score_without_genome))
+        c += 1
+        print('Done with ' + str(c) + ' targtes.')
 
     sys.stdout = open('all_scores', 'w')
     for score in list_grna_scores:
-        print(score[0] + ' ' + score[1])
+        print(str(score[0]) + ' ' + str(score[1]))
     sys.stdout.close()
 
 if __name__ == "__main__":
