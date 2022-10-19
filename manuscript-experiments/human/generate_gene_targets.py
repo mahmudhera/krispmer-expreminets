0f847d0cddae47892ea3bd24d64739a427305ac7
Mahmudur <mahmudhera93@gmail.com>
2022-10-19 17:10:16 -0400

    mostly final

--- manuscript-experiments/human/generate_gene_targets.py
+++ manuscript-experiments/human/generate_gene_targets.py
@@ -24,40 +24,38 @@ if __name__ == "__main__":
 
     bash_file = open('script_for_protein_coding_targets.sh', 'w')
 
-    '''
-    df_protein_coding_genes = df[ df['ftype'] == 'protein coding gene' ]
-    bash_file = open('script_for_protein_coding_targets.sh', 'w')
+    contig_names = df_genes[0].tolist()
+    start_positions = df_genes[3].tolist()
+    end_positions = df_genes[4].tolist()
 
-    print('All seq IDs...')
-    for seq_record in SeqIO.parse(fasta_filename, "fasta"):
-        print(seq_record.id)
+    all_genes = list( zip(contig_names, start_positions, end_positions) )
+    random.shuffle(all_genes)
 
-    chr_names = df_protein_coding_genes['chr'].tolist()
-    start_pos = df_protein_coding_genes['start_coord'].tolist()
-    end_pos = df_protein_coding_genes['end_coord'].tolist()
-
-    list_positions = list(zip(chr_names, start_pos, end_pos))
-    random.shuffle(list_positions)
-    target_id = 1
-    count = 0
-    for chr_name, start, end in list_positions:
-        start = int(start)
-        end = int(end)
+    print('indexing genome file...')
+    record_dict = SeqIO.index(fasta_filename, "fasta")
+    print('indexing complete!')
+    
+    num_targets_generated = 0
+    for (contig_id, start_pos, end_pos) in all_genes:
+        start = int(start_pos)
+        end = int(end_pos)
         if end < 150 or end-150 <= start:
             continue
         target_start = random.randint(start, end-150)
-        for seq_record in SeqIO.parse(fasta_filename, "fasta"):
-            if seq_record.id == chr_to_contig_id[chr_name]:
-                fname = 'target_gene_' + str(target_id) + '_' + chr_name + "_" + str(target_start) + ':' + str(target_start+150) + '.fasta'
-                f = open(fname, 'w')
-                f.write('> ' + 'target_gene_' + str(target_id) + '_' + chr_name + "_" + str(target_start) + ':' + str(target_start+150) + '\n')
-                f.write(str(seq_record.seq[target_start:target_start+150]))
-                f.close()
-                target_id = target_id + 1
-                bash_file.write('krispmer ' + reads_filename + ' ' + fname + ' ' + 'scores_' + fname + ' ' + str(hd) + ' -n -J mer_counts.jf -H k_spectrum_histo\n')
-        count += 1
-        if count >= num_targets:
+        seq_record = record_dict[contig_id]
+        target_string = seq_record.seq[target_start : target_start+150]
+
+        fname = 'target_gene_' + chr_name + "_" + str(target_start) + ':' + str(target_start+150) + '.fasta'
+        f = open(fname, 'w')
+        f.write('> ' + fname + '\n')
+        f.write(target_string)
+        f.close()
+        bash_file.write('krispmer ' + reads_filename + ' ' + fname + ' ' + 'scores_' + fname + ' ' + str(hd) + ' -n -J mer_counts.jf -H k_spectrum_histo\n')
+
+        print('Num of targets generated: ' + str(num_targets_generated))
+
+        num_targets_generated += 1
+        if num_targets_generated == num_targets:
             break
 
     bash_file.close()
-    '''
