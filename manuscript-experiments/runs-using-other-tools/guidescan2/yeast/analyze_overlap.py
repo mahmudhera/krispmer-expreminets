0546319cc3c3f758ce359ffa402c9e74b6f8a9cc
Mahmudur <mahmudhera93@gmail.com>
2022-11-17 23:26:38 -0500

    code for filenames

--- manuscript-experiments/runs-using-other-tools/guidescan2/yeast/analyze_overlap.py
+++ manuscript-experiments/runs-using-other-tools/guidescan2/yeast/analyze_overlap.py
@@ -6,14 +6,14 @@ targets_dir_name = 'inputs'
 gs_out_dir_name = 'gs_out'
 kr_out_dir_name = 'krispmer_targets'
 
-def generate_gs_out_filename():
+def generate_gs_out_filename(tgt_name):
     # given a target filename, generate guidescan output filename
-    return None
+    return join(gs_out_dir_name, 'gs_out_'+tgt_name)
 
 
 def generate_krispmer_out_filename():
     # given a target filename, generate krispmer output filename
-    return None
+    return join(kr_out_dir_name, 'scores_'+tgt_name)
 
 
 def find_overlap_given_target_filename(target_filename):
@@ -30,11 +30,12 @@ def find_all_targets():
 
 if __name__ == '__main__':
     target_filenames_list = find_all_targets()
-    print(target_filenames_list[:5])
-    print(target_filenames_list[-5:])
     exit(-1)
     summary = []
     for target_filename in target_filenames_list:
+        target_name = target_filename.split('/')[-1]
+        print( generate_gs_out_filename(target_name) )
+        print( generate_krispmer_out_filename(target_name) )
         only_krispmer, common, only_guidescan = find_overlap_given_target_filename(target_filename)
         summary.append( (target_filename, only_krispmer, common, only_guidescan) )
     df = DataFrame(summary, columns=['target_filename', 'only_in_kr', 'in_both', 'only_in_gs'])
