e3a294412b01c4d6f7c3bdb2c8fa594fabdf1dcf
Mahmudur <mahmudhera93@gmail.com>
2023-07-12 22:39:30 -0400

    added guidescan guides for yeast, ready to test

--- manuscript-experiments/analyses/perfect_off_targets/aggregate_off_target_counts_guidescan.py
+++ manuscript-experiments/analyses/perfect_off_targets/aggregate_off_target_counts_guidescan.py
@@ -52,7 +52,7 @@ def get_all_files(directory):
 
 def generate_scores_filename(target_filename_with_path):
     splitted = target_filename_with_path.split('/')
-    return '/'.join(splitted[:-2] + ['krispmer_guides','scores_'+splitted[-1]])
+    return '/'.join(splitted[:-2] + ['guidescan_guides','scores_'+splitted[-1]+'_extracted'])
 
 def main():
     # take command line arguments
