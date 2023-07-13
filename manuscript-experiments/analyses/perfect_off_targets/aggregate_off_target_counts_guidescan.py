ed3f262e78f5951f4ef92362836ac8025f41cf09
Mahmudur <mahmudhera93@gmail.com>
2023-07-12 22:34:05 -0400

    added code for guidescan

--- manuscript-experiments/analyses/perfect_off_targets/aggregate_off_target_counts_guidescan.py
+++ manuscript-experiments/analyses/perfect_off_targets/aggregate_off_target_counts_guidescan.py
@@ -56,11 +56,10 @@ def generate_scores_filename(target_filename_with_path):
 
 def main():
     # take command line arguments
-    parser = argparse.ArgumentParser(description='Process krispmer results.')
+    parser = argparse.ArgumentParser(description='Process guidescan2 results.')
     parser.add_argument('target_dir_name', help='Full path to the directory containing all target sequences')
     parser.add_argument('grna_dir_name', help='Full path to the directory containing all grnas')
     parser.add_argument('genome_jf_filename', help='Full path to the jellyfish counted file for the genome')
-    parser.add_argument('cutoff_score', type=float, help='cut off score to consider')
 
     args = parser.parse_args()
 
@@ -78,30 +77,58 @@ def main():
     # list all target and grna files
 
     for target_file, grna_file in target_files_and_grna_files:
-        # list all grnas
+        # read file, if 0 lines, then skip
         grna_file = open(grna_file, 'r')
         lines = grna_file.readlines()
         grna_file.close()
+        if len(lines) < 2:
+            continue
+
+        # if > 1 lines then skip first line
+        # every line after that is a grna
         grna_list = []
         for line in lines[1:]:
-            if float(line.split(',')[2]) > cutoff_score:
-                continue
-            grna_list.append( (line.split(',')[0], line.split(',')[1]) )
-        #print(grna_list)
+            grna_list.append( line.upper() )
 
-        # get the target sequence
+        # load the target seq
         tgt_f = open(target_file, 'r')
         lines = tgt_f.readlines()
         tgt_f.close()
         target_sequence = ''.join( [line.strip().upper() for line in lines[1:]] )
         #print(target_sequence)
 
-        for tgt_in_plus, tgt_in_minus in grna_list:
-            num_occurrences_in_target = target_sequence.count(tgt_in_plus)+target_sequence.count(tgt_in_minus)
-            num_occurrences_in_genome = qf_genome[jellyfish.MerDNA(tgt_in_plus)] + qf_genome[jellyfish.MerDNA(tgt_in_minus)]
+        alphabet = 'ACGT'
+        # for every grna
+        for grna in grna_list:
+            # skip if len != 23
+
+            if not len(grna) == 23:
+                continue
+            candidate_grnas = []
+            # generate all representations of the grna (N = ACGT)
+            for nucleotide in alphabet:
+                candidate_grna = grna.replace('N', nucleotide)
+                # keep if this is in the target seq
+                if candidate_grna in target_sequence or reverse_complement(candidate_grna) in target_sequence:
+                    candidate_grnas.append(candidate_grna)
+
+            # count their appearences
+            num_occurrences_in_target = 0
+            for grna in candidate_grnas:
+                num_occurrences_in_target += target_sequence.count(grna)+target_sequence.count(reverse_complement(grna))
+
+            # generate adj mers of all representations: keep them in a set
+            # count their appearences
+            num_occurrences_in_genome = 0
+            for grna in candidate_grnas:
+                num_occurrences_in_genome += qf_genome[jellyfish.MerDNA(grna)] + qf_genome[jellyfish.MerDNA(grna)]
+
+            # find off target count
             ot_count_0_mismatch = max(0, num_occurrences_in_genome - num_occurrences_in_target)
 
-            sequences_with_one_distance = set(generate_adjacent_mers(tgt_in_plus, 1))
+            sequences_with_one_distance = set([])
+            for grna in candidate_grnas:
+                sequences_with_one_distance.update(generate_adjacent_mers(grna, 1))
             num_occurrences_in_target = 0
             num_occurrences_in_genome = 0
             for potential_off_target in sequences_with_one_distance:
@@ -109,13 +136,15 @@ def main():
                 num_occurrences_in_genome += qf_genome[jellyfish.MerDNA(potential_off_target)] + qf_genome[jellyfish.MerDNA(reverse_complement(potential_off_target))]
             ot_count_1_mismatch = max(0, num_occurrences_in_genome - num_occurrences_in_target)
 
-            sequences_with_two_distance = set(generate_adjacent_mers(tgt_in_plus, 2))
+            sequences_with_one_distance = set([])
+            for grna in candidate_grnas:
+                sequences_with_one_distance.update(generate_adjacent_mers(grna, 2))
             num_occurrences_in_target = 0
             num_occurrences_in_genome = 0
             for potential_off_target in sequences_with_one_distance:
                 num_occurrences_in_target += target_sequence.count(potential_off_target) + target_sequence.count(reverse_complement(potential_off_target))
                 num_occurrences_in_genome += qf_genome[jellyfish.MerDNA(potential_off_target)] + qf_genome[jellyfish.MerDNA(reverse_complement(potential_off_target))]
-            ot_count_2_mismatch = max(0, num_occurrences_in_genome - num_occurrences_in_target)
+            ot_count_1_mismatch = max(0, num_occurrences_in_genome - num_occurrences_in_target)
 
             print(str(target_file).split('/')[-1], tgt_in_plus, ot_count_0_mismatch, ot_count_1_mismatch, ot_count_2_mismatch)
             # <target_filename, grna, ot_count, type> add this to the summary file
