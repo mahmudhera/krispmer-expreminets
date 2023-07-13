493bbb6cccb72390235feeda160131932aee652c
Mahmudur <mahmudhera93@gmail.com>
2023-07-12 22:41:59 -0400

    fixed naming

--- manuscript-experiments/analyses/perfect_off_targets/aggregate_off_target_counts_guidescan.py
+++ manuscript-experiments/analyses/perfect_off_targets/aggregate_off_target_counts_guidescan.py
@@ -52,7 +52,7 @@ def get_all_files(directory):
 
 def generate_scores_filename(target_filename_with_path):
     splitted = target_filename_with_path.split('/')
-    return '/'.join(splitted[:-2] + ['guidescan_guides','scores_'+splitted[-1]+'_extracted'])
+    return '/'.join(splitted[:-2] + ['guidescan_guides','gs_out_'+splitted[-1]+'_extracted'])
 
 def main():
     # take command line arguments
