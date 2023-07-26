6a1f3cce0e84a0d0484461e27ea9b38c429d1572
Mahmudur <mahmudhera93@gmail.com>
2023-07-26 17:45:03 -0400

    added script

---
+++ manuscript-experiments/runs-using-other-tools/crispor/using_local_installation/human/script_for_chm13.sh
@@ -0,0 +1,3 @@
+python /var/www/html/crispor.py hg38 chm13_targets/target_1.fasta results/target_1.fasta_guides.tsv -o results/target_1.fasta_off_targets.tsv
+python /var/www/html/crispor.py hg38 chm13_targets/target_2.fasta results/target_2.fasta_guides.tsv -o results/target_2.fasta_off_targets.tsv
+python /var/www/html/crispor.py hg38 chm13_targets/target_3.fasta results/target_3.fasta_guides.tsv -o results/target_3.fasta_off_targets.tsv
