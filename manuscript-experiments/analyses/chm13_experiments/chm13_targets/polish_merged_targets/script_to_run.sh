c168b661903abb715202dce6d0292e5cbb4934ff
atifrahman <atif.bd@gmail.com>
2023-08-04 14:20:50 -0700

    added polished present_vs_absent targets

---
+++ manuscript-experiments/analyses/chm13_experiments/chm13_targets/polish_merged_targets/script_to_run.sh
@@ -0,0 +1,58 @@
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_19_ENST00000011653.9.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_19_ENST00000011653.9.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_7_ENST00000023897.10.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_7_ENST00000023897.10.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_18_ENST00000168712.3.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_18_ENST00000168712.3.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_2_ENST00000187397.8.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_2_ENST00000187397.8.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_14_ENST00000005226.12.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_14_ENST00000005226.12.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_23_ENST00000007969.12.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_23_ENST00000007969.12.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_24_ENST00000075503.8.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_24_ENST00000075503.8.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_21_ENST00000010132.6.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_21_ENST00000010132.6.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_22_ENST00000009180.10.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_22_ENST00000009180.10.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_20_ENST00000008527.10.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_20_ENST00000008527.10.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_8_ENST00000046794.10.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_8_ENST00000046794.10.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_27_ENST00000080059.12.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_27_ENST00000080059.12.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_15_ENST00000016913.8.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_15_ENST00000016913.8.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_25_ENST00000010404.6.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_25_ENST00000010404.6.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_13_ENST00000182290.9.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_13_ENST00000182290.9.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_12_ENST00000176183.6.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_12_ENST00000176183.6.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_4_ENST00000164024.5.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_4_ENST00000164024.5.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_3_ENST00000072516.7.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_3_ENST00000072516.7.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_26_ENST00000081029.8.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_26_ENST00000081029.8.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_6_ENST00000033079.7.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_6_ENST00000033079.7.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_10_ENST00000166534.8.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_10_ENST00000166534.8.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_28_ENST00000188312.7.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_28_ENST00000188312.7.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_17_ENST00000164133.7.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_17_ENST00000164133.7.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_1_ENST00000170447.12.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_1_ENST00000170447.12.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_16_ENST00000005286.8.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_16_ENST00000005286.8.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_5_ENST00000169293.10.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_5_ENST00000169293.10.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_9_ENST00000029410.10.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_9_ENST00000029410.10.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_29_ENST00000164305.10.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_29_ENST00000164305.10.fasta
+java -Xmx16G -jar /home/atif/krispmer/experiments/krispmer-expreminets/manuscript-experiments/human/krispmer_temp/pilon-1.23.jar --genome ../present_vs_absent_targets/target_11_ENST00000039989.9.fasta --unpaired bam_sorted.bam
+mv pilon.fasta target_11_ENST00000039989.9.fasta
