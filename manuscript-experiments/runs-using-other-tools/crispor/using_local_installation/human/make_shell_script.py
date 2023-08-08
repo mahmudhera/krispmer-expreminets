a9e755c549048d835944bcbd4717d7a2d560ad4c
Mahmudur <mahmudhera93@gmail.com>
2023-08-08 00:42:53 -0400

    ready to run crispor on chm13 present_v_absent targets

--- manuscript-experiments/runs-using-other-tools/crispor/using_local_installation/human/make_shell_script.py
+++ manuscript-experiments/runs-using-other-tools/crispor/using_local_installation/human/make_shell_script.py
@@ -2,12 +2,13 @@
 from os import listdir
 from os.path import *
 
-script_file = open('script_for_human_crispor.sh', 'w')
+script_file = open('script_chm13_crispor_present_vs_absent.sh', 'w')
+target_dir_name = 'chm13_targets_present_vs_absent'
+results_dir_name = 'chm13_results_present_vs_absent'
 
 targets = [f for f in listdir('./human_targets') if isfile(join('./human_targets', f))]
 print(targets[:10])
 for target in targets:
-    cmd = f'python /var/www/html/crispor.py hg38 human_targets/{target} results/{target}_guides.tsv -o results/{target}_off_targets.tsv'
+    cmd = f'python /var/www/html/crispor.py hg38 {target_dir_name}/{target} {results_dir_name}/{target}_guides.tsv'
     script_file.write(cmd + '\n')
-
 script_file.close()
