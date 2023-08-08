2221499c3052b7e1c45bc08b6f89e7635b2f1104
Mahmudur <mahmudhera93@gmail.com>
2023-08-08 17:44:53 -0400

    added plots for 29 present vs absent targets

--- manuscript-experiments/analyses/chm13_experiments/present_vs_absent_targets_and_results/summarize_ot_grnas.py
+++ manuscript-experiments/analyses/chm13_experiments/present_vs_absent_targets_and_results/summarize_ot_grnas.py
@@ -4,6 +4,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import csv
 import numpy as np
+import os
 
 # Step 2: Choose the font 'Times New Roman'
 title_font = {'fontname': 'Arial', 'fontsize': 12}
@@ -14,15 +15,12 @@ tick_label_font = {'fontname': 'Arial', 'fontsize': 10}
 plt.rcParams['font.family'] = 'sans-serif'  # Set the default font family
 plt.rcParams['font.sans-serif'] = 'Arial'  # Set the default font to Times New Roman
 
-file_str = 'summary_'
+file_str = 'ot_summary_chm13_'
 #methods = ['crispor', 'guidescan2', 'krispmer', 'krispmer2.0', 'krispmer3.0']
 methods = ['crispor', 'guidescan2', 'krispmer']
-num_targets = 3
 ot_categories = ['0 mismatch OT', '1 mismatch OT', '2/more mismatch OT']
-transcript_ids = ['ENST00000375050.6', 'ENST00000568530.5', 'ENST00000261405.10']
 
-for target_id in range(num_targets):
-
+for target_filename in os.listdir('targets'):
     off_target_counts_0_mm = list([0]*len(methods))
     off_target_counts_1_mm = list([0]*len(methods))
     off_target_counts_2_or_more_mm = list([0]*len(methods))
@@ -31,7 +29,7 @@ for target_id in range(num_targets):
         method_name = methods[i]
         aggregated_filename = f'{file_str}{method_name}'
         df = pd.read_csv(aggregated_filename, delimiter=' ', header=None, names=['target_name', 'grna', 'a', 'b', 'c'])
-        df = df[ df['target_name']==f'target_{target_id+1}.fasta' ]
+        df = df[ df['target_name']==target_filename ]
         targets = df['target_name'].tolist()
         grnas = df['grna'].tolist()
         a_s = df['a'].tolist()
@@ -51,7 +49,8 @@ for target_id in range(num_targets):
             if c > 0:
                 off_target_counts_2_or_more_mm[i] += 1
 
-    pdf_filename = f'ot_summary_plot_target_{target_id+1}.pdf'
+    splitted = target_filename.split('.')[0]
+    pdf_filename = f'ot_summary_pdfs/ot_summary_{splitted}.pdf'
     fig, ax = plt.subplots()
     ax.bar(methods, off_target_counts_0_mm, label='0 mismatch to OTs')
     ax.bar(methods, off_target_counts_1_mm, bottom=off_target_counts_0_mm, label='1 mismatch to OTs')
@@ -59,6 +58,7 @@ for target_id in range(num_targets):
     ax.legend(loc='upper right')
     plt.xlabel('gRNA finding tools')
     plt.ylabel('Number of gRNAs identified')
-    plt.title(f'gRNAs identified by tools for transcript id {transcript_ids[target_id]}')
+    splitted = target_filename.split('_')[-1].split('.')[0]
+    plt.title(f'gRNAs identified by tools for target: {splitted}')
     plt.savefig(pdf_filename)
     plt.clf()
