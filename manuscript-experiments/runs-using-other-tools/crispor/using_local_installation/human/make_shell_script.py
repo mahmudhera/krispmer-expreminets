92b3bb27a696e5ce70786a64cfc69e96bda75e60
Mahmudur <mahmudhera93@gmail.com>
2023-08-08 01:06:29 -0400

    fixed the script

--- manuscript-experiments/runs-using-other-tools/crispor/using_local_installation/human/make_shell_script.py
+++ manuscript-experiments/runs-using-other-tools/crispor/using_local_installation/human/make_shell_script.py
@@ -6,7 +6,7 @@ script_file = open('script_chm13_crispor_present_vs_absent.sh', 'w')
 target_dir_name = 'chm13_targets_present_vs_absent'
 results_dir_name = 'chm13_results_present_vs_absent'
 
-targets = [f for f in listdir('./human_targets') if isfile(join('./human_targets', f))]
+targets = [f for f in listdir(target_dir_name) if isfile(join(target_dir_name, f))]
 print(targets[:10])
 for target in targets:
     cmd = f'python /var/www/html/crispor.py hg38 {target_dir_name}/{target} {results_dir_name}/{target}_guides.tsv'
