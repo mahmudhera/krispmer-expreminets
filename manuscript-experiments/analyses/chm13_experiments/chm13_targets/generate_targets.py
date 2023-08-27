48b5f5efbb6ca44b2dc5a4fa2c5081a7445da7d3
Mahmudur <mahmudhera93@gmail.com>
2023-08-27 19:55:56 -0400

    generate records for multiple_vs_single

--- manuscript-experiments/analyses/chm13_experiments/chm13_targets/generate_targets.py
+++ manuscript-experiments/analyses/chm13_experiments/chm13_targets/generate_targets.py
@@ -1,19 +1,18 @@
 import pandas as pd
 from Bio import SeqIO
+import random
 
-summarized_filename = 'mart_export_present_v_absent.txt'
+random.seed(0)
+
 exons_filename = '/home/atif/human_assemblies_kmer_count/exons.fa'
+handpicked_transcript_ids = ['ENST00000375050', 'ENST00000261405', 'ENST00000568530', 'ENST00000375050', 'ENST00000288050', 'ENST00000261405', 'ENST00000351205', 'ENST00000380742', 'ENST00000374312']
 
 if __name__ == '__main__':
-    df_summary = pd.read_csv(summarized_filename, delimiter='\t', header=None)
-    print(df_summary)
-    transcript_ids = df_summary[3].tolist()
-
     target_counter = 1
 
     with open(exons_filename) as handle:
         for record in SeqIO.parse(handle, "fasta"):
-            for transcript_id in transcript_ids[:30]:
+            for transcript_id in handpicked_transcript_ids:
                 if transcript_id in record.id:
                     target_name = f'target_{target_counter}_{transcript_id}.fasta'
                     target_counter += 1
@@ -21,3 +20,18 @@ if __name__ == '__main__':
                     f.write('>' + target_name + '\n')
                     f.write(str(record.seq) + '\n')
                     f.close()
+
+        all_record_ids = []
+        for record in SeqIO.parse(handle, "fasta"):
+            all_record_ids.append(str(record.id))
+        random.shuffle(all_record_ids)
+        selected_record_ids = all_record_ids[:100]
+
+        for record in SeqIO.parse(handle, "fasta"):
+            if str(record.id) in selected_record_ids:
+                target_name = f'target_{target_counter}_{transcript_id}.fasta'
+                target_counter += 1
+                f = open(target_name, 'w')
+                f.write('>' + target_name + '\n')
+                f.write(str(record.seq) + '\n')
+                f.close()
