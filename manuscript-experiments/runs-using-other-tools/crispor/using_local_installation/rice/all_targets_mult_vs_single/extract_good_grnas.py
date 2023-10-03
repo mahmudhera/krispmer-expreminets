12e378232d5988f82523f0aba4104d51a9494407
mahmudhera <mahmudhera93@gmail.com>
2023-10-03 19:54:57 +0000

    added updated script

--- manuscript-experiments/runs-using-other-tools/crispor/using_local_installation/rice/all_targets_mult_vs_single/extract_good_grnas.py
+++ manuscript-experiments/runs-using-other-tools/crispor/using_local_installation/rice/all_targets_mult_vs_single/extract_good_grnas.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import subprocess
+import os
 
 df = pd.read_csv('multiple_v_single.txt', header=None, delimiter='\t')
 
 i = 1
 total = len(df[0].tolist())
-for kmer in tqdm(df[0].tolist()):
+for kmer in df[0].tolist():
     print('Handling '+str(i)+'/'+str(total)+':')
     f = open('tmp.fasta', 'w')
     f.write('>target\n')
     f.write(kmer+'\n')
     f.close()
     cmd = 'python /var/www/html/crispor.py pz9Osativa tmp.fasta tmp.tsv'
-    subprocess.run(cmd.split(' '), stdout=open(os.devnull, 'wb'))
+    subprocess.call(cmd.split(' '), stdout=open(os.devnull, 'wb'), stderr=open(os.devnull, 'wb'))
     f = open('tmp.tsv', 'r')
     line = f.readlines()[1]
     f.close()
