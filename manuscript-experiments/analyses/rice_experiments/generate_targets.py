5759fce2af348eaa2f7ef80c0c70c3d7d4826976
Mahmudur <mahmudhera93@gmail.com>
2023-10-16 17:14:10 -0400

    added code to generate rice mult v single targets

--- manuscript-experiments/analyses/rice_experiments/generate_targets.py
+++ manuscript-experiments/analyses/rice_experiments/generate_targets.py
@@ -4,37 +4,27 @@ import random
 
 random.seed(0)
 
-exons_filename = '/home/atif/human_assemblies_kmer_count/exons.fa'
+candidate_genes_list_filename = '~/oryza-sativa-genomes-and-counts/kmer_counts/genes_multiple_v_single_analysis_all.txt'
+sequences_filename = '~/oryza-sativa-genomes-and-counts/kmer_counts/cds_primary.fa'
 
 if __name__ == '__main__':
+    # read all candidate genes list
+    f = open(candidate_genes_list_filename, 'r')
+    lines = f.readlines()
+    f.close()
+    gene_names_list = [ line.strip() for line in lines ]
+    random.shuffle(gene_names_list)
+    gene_names_list = gene_names_list[:100]
+
     target_counter = 1
 
     with open(exons_filename) as handle:
         for record in SeqIO.parse(handle, "fasta"):
-            for transcript_id in handpicked_transcript_ids:
-                if transcript_id in record.id:
-                    target_name = f'target_{target_counter}_{transcript_id}.fasta'
+            for gene_name in gene_names_list:
+                if gene_name in record.id:
+                    target_name = f'target_{target_counter}_{gene_name}.fasta'
                     target_counter += 1
                     f = open(target_name, 'w')
                     f.write('>' + target_name + '\n')
                     f.write(str(record.seq) + '\n')
                     f.close()
-
-    with open(exons_filename) as handle:
-        all_record_ids = []
-        for record in SeqIO.parse(handle, "fasta"):
-            all_record_ids.append(str(record.id))
-        random.shuffle(all_record_ids)
-        selected_record_ids = all_record_ids[:100]
-
-        print(selected_record_ids)
-    with open(exons_filename) as handle:
-        for record in SeqIO.parse(handle, "fasta"):
-            if str(record.id) in selected_record_ids:
-                new_transcript_id = str(record.id).split('_')[-1]
-                target_name = f'target_{target_counter}_{new_transcript_id}.fasta'
-                target_counter += 1
-                f = open(target_name, 'w')
-                f.write('>' + target_name + '\n')
-                f.write(str(record.seq) + '\n')
-                f.close()
