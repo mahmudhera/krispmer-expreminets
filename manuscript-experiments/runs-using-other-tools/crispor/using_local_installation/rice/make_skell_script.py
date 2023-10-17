805c903aec20c77f0f53ef06ad7d7e1fe08dd6bb
Mahmudur <mahmudhera93@gmail.com>
2023-10-17 00:27:18 -0400

    added script to run crispor, needs correction

---
+++ manuscript-experiments/runs-using-other-tools/crispor/using_local_installation/rice/make_skell_script.py
@@ -0,0 +1,13 @@
+from os import listdir
+from os.path import *
+
+script_file = open('script_rice.sh', 'w')
+target_dir_name = 'multiple_vs_single_targets'
+results_dir_name = 'multiple_vs_single_results'
+
+targets = [f for f in listdir(target_dir_name) if isfile(join(target_dir_name, f))]
+print(targets[:10])
+for target in targets:
+    cmd = f'python /var/www/html/crispor.py hg38 {target_dir_name}/{target} {results_dir_name}/{target}_guides.tsv'
+    script_file.write(cmd + '\n')
+script_file.close()
